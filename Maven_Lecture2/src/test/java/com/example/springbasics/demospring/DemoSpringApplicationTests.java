f782b3da4f28580288c81a2f7f3ae92eb9bc34ba
Piyush Aggarwal <piyush5807@gmail.com>
2020-07-06 09:41:41 +0530

    added maven project

---
+++ Maven_Lecture2/src/test/java/com/example/springbasics/demospring/DemoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbasics.demospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
