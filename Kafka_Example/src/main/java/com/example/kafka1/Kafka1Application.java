57d99fe4f6f26e420ae363d5795ff17a80d65767
Shashi Bhushan Kumar <shashibk1993@gmail.com>
2020-08-25 13:22:20 +0530

    Added Redis, Kafka examples and major project

---
+++ Kafka_Example/src/main/java/com/example/kafka1/Kafka1Application.java
@@ -0,0 +1,13 @@
+package com.example.kafka1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kafka1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kafka1Application.class, args);
+	}
+
+}
